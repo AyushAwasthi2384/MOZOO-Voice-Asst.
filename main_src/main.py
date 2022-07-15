50880f6c3cfe5ef8679e41e37bc5b8eec7f71995
AyushAwasthi2384 <ayushawasthicoding@gmail.com>
2022-07-15 17:08:08 +0530

    main-first-commit

--- main_src/main.py
+++ main_src/main.py
@@ -1,6 +1,26 @@
-# hello bro
+import pyttsx3 as p
+import speech_recognition as sr
 
-print("hello world")
-print("hello world")
-# new commit
-print("hello")
+# initialization of the text to speech module
+engine = p.init()
+engine.setProperty('rate', 175)
+
+voices = engine.getProperty('voices')
+engine.setProperty('voice',voices[0].id)
+
+# # The output...
+engine.say("Hello World, my name is mozoo.")
+engine.say("Hi how are you?")
+engine.runAndWait()
+
+
+# initialization of the speech recognition module
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    r.energy_threshold = 10000
+    r.adjust_for_ambient_noise(source, 1.2)
+    print("listening...")
+    audio = r.listen(source)
+    text = r.recognize_google(audio)
+    print(text)
