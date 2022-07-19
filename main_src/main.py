68eb241390a61840b8d879312b2ab0b74bd6eb17
AyushAwasthi2384 <ayushawasthicoding@gmail.com>
2022-07-19 18:29:20 +0530

    19-july

--- main_src/main.py
+++ main_src/main.py
@@ -1,26 +1,33 @@
 import pyttsx3 as p
 import speech_recognition as sr
+import keyboard
+import sys
 
-# initialization of the text to speech module
-engine = p.init()
-engine.setProperty('rate', 175)
+print("START")
+key = keyboard.read_key()
 
-voices = engine.getProperty('voices')
-engine.setProperty('voice',voices[0].id)
+if key == "enter":
+    # initialization of the text to speech module
+    engine = p.init()
+    engine.setProperty('rate', 175)
 
-# # The output...
-engine.say("Hello World, my name is mozoo.")
-# engine.say("Hi how are you?")
-engine.runAndWait()
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[1].id)
 
+    # The output...
+    engine.say("Hello World, my name is mozoo.")
+    # engine.say("Hi how are you?")
+    engine.runAndWait()
 
-# initialization of the speech recognition module
-r = sr.Recognizer()
+    # initialization of the speech recognition module
+    r = sr.Recognizer()
 
-with sr.Microphone() as source:
-    r.energy_threshold = 10000
-    r.adjust_for_ambient_noise(source, 1.2)
-    print("listening...")
-    audio = r.listen(source)
-    text = r.recognize_google(audio)
-    print(text)
+    with sr.Microphone() as source:
+        r.energy_threshold = 10000
+        r.adjust_for_ambient_noise(source, 1.2)
+        print("listening...")
+        audio = r.listen(source)
+        text = r.recognize_google(audio)
+        print(text)
+else:
+    print("END")
